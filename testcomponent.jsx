e219d0263b26512f86149e44e5a49b3f1c94929b
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2018-04-19 14:50:34 +1000

    Add the actual test component

---
+++ testcomponent.jsx
@@ -0,0 +1,7 @@
+import React, { PureComponent } from 'react';
+
+export default class TestComponent extends PureComponent {
+  render() {
+    return <span>asdfasd</span>;
+  }
+}
--- testloadable.jsx
+++ testloadable.jsx
@@ -1,3 +1,3 @@
 import loadable from 'react-universal-component';
 
-export default loadable(import('./component.jsx'));
+export default loadable(import('./testcomponent.jsx'));
